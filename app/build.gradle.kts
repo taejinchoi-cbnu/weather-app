c0ce2913914a6ba64398ac345f0acc73c7acd4bb
gcotsh <ymin0120@gmail.com>
2025-03-21 10:00:54 +0900

    check to add user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
 
     //  lottie animations
     implementation("com.airbnb.android:lottie:6.1.0")
-
     // GSON converter
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // retrofit
